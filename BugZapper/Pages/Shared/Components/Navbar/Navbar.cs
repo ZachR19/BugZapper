a3af2c0a56349fd8cd3ff0de9c9de20c49945dbd
Zach Raudebaugh <raudebaughzach@gmail.com>
2020-08-05 22:35:58 -0400

    Changed the navbar to a ViewComponent

---
+++ BugZapper/Pages/Shared/Components/Navbar/Navbar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugZapper.Pages.Shared.Components.Navbar
+{
+    public class Navbar : ViewComponent
+    {
+        public Navbar()
+        {
+
+        }
+
+        public IViewComponentResult Invoke(string URL)
+        {
+            var url = Request.GetDisplayUrl().ToLower();
+            
+            if (url.Contains("project"))
+                return View("Navbar", "projects");
+            else if (url.Contains("team"))
+                return View("Navbar", "teams");
+
+            return View("Navbar", "home");
+        }
+
+    }
+}
